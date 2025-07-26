d879393b1d88fa2b1dc3115bb8a8f1ecbc5a88cf
shettyharshith33 <harshithshetty033@gmail.com>
2025-07-26 08:45:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/sharathkolpe/unused/Demo Clone.kt
@@ -0,0 +1,10 @@
+package com.sharathkolpe.unused
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Demo(modifier: Modifier = Modifier) {
+    Text("Hello")
+}
